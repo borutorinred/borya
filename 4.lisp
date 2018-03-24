5847711f2e1bf3c9f834b1d2ab249b35861b09b8
borutorinred <37692980+borutorinred@users.noreply.github.com>
2018-03-24 09:44:20 +0700

    Update 4.lisp

--- 4.lisp
+++ 4.lisp
@@ -1,7 +1,4 @@
-(DEFUN APPEND1 (X Y)
-       (COND ((CDR X) (CONS (CAR X) (APPEND1 (CDR X) Y)))
-             (T (CONS (CAR X) Y))))
-(DEFUN REVERSE1 (X)
-       (COND ((CDR (CDR X)) (APPEND1 (REVERSE1 (CDR X)) (LIST (CAR X))))
-             (T (LIST (CAR (CDR X)) (CAR X)))))
-(PRINT (REVERSE1 '(A B C D)))
+(defun REVERSE1 (x) (cond (x (APPEND1 (REVERSE1 (cdr x)) (cons (car x) nil)))
+             
+                          (T ())))
+(print (REVERSE1 '(A B C)))
