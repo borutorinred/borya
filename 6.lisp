5d3279d3afa1d69c6a7041a9bbabcc4c6371ebb2
borutorinred <37692980+borutorinred@users.noreply.github.com>
2018-04-14 12:03:13 +0700

    Update 6.lisp

--- 6.lisp
+++ 6.lisp
@@ -2,6 +2,6 @@
                   (COND((ATOM AIDS ) AIDS)
                    ((ATOM (CAR AIDS)) (CONS (CAR AIDS))(ATOMS (CDR AIDS) ) ))
                    (T ( APPEND ( ATOMS ( CAR AIDS))( ATOMS ( CDR AIDS ) ))))
-          )
+          
 
 (PRINT (ATOMS '((A B) C NIL (D (E F G)))))
